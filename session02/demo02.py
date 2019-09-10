93b112df6f87b5959e7521fff535893488543184
JulieZhang0102 <jzhang10@babson.edu>
2019-09-10 17:23:48 -0400

    Create demo02.py

---
+++ session02/demo02.py
@@ -0,0 +1,29 @@
+# print('Hello, world!')
+
+# print('Hey Jude, dont\'t make it bad')
+
+# print('The total number of medals in Rio 2016 is', 46+37+38, '.')
+
+# print('46+37+38=', 46+37+38)
+
+# name = input()
+# print('Hello, ', name)
+
+# message = 'I did something incredible today!'
+# n = 100
+# pi = 3.14
+
+# first_name = 'John'
+# last_name = 'Zhang'
+# print('Hello, ' + first_name + ' ' + last_name + '.')
+
+# print('Pi equals {:.5f}.'.format(3.1415926))
+
+# print('Age: {age}; Gender: {sex}'.format(sex='Female', age = 20))
+# print('Age: {}; Gender: {}'.format('Female', 20))
+
+sex = 'Female'
+age = 20
+
+print(f'Age: {age}; Gender: {sex}')
+print('Age: {age}; Gender: {sex}'.format(sex=sex, age=age))
