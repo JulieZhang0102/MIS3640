eb1751dd456e53885b24e1e847d4ccfb35b547e8
JulieZhang0102 <jzhang10@babson.edu>
2019-09-12 17:07:33 -0400

    Update demo02.py

--- session02/demo02.py
+++ session02/demo02.py
@@ -22,8 +22,90 @@
 # print('Age: {age}; Gender: {sex}'.format(sex='Female', age = 20))
 # print('Age: {}; Gender: {}'.format('Female', 20))
 
-sex = 'Female'
-age = 20
+# sex = 'Female'
+# age = 20
 
-print(f'Age: {age}; Gender: {sex}')
-print('Age: {age}; Gender: {sex}'.format(sex=sex, age=age))
+# print(f'Age: {age}; Gender: {sex}')
+# print('Age: {age}; Gender: {sex}'.format(sex=sex, age=age))
+
+# import math
+
+# result1 = 11.5
+# print(result1- int(11.5))
+# print(math.floor(-2.5))
+# print(-5//2)
+
+# number = 2**100000
+
+# str_of_number = str(number)
+# print(len(str_of_number))
+
+# print(len(str(2**100000)))
+
+# import math
+
+# print(math.pi)
+# print(math.e)
+# print(math.sqrt(17))
+
+# import random
+# print(random.random())
+# print(random.choice(['Julie', 'Shirley', 'Rachel', 'Amy']))
+
+# print('I\'m \"ok\".')
+# print('I\'m learning\nPython')
+
+# print('\\\n\\')
+# print(r'\\\n\\')
+# print('''hello
+# ... folks
+# ... hi''')
+
+# print(3>2)
+# print(True and True)
+# print(True and False)
+# print(False and False)
+# print(5>3 and 3<1)
+
+# print(True or True)
+# print(True or False)
+# print(False or False)
+# print(5>3 or 3<1)
+# print(not True)
+
+# age = 20
+
+# if age >= 21:
+#     print('Yes, you can.')
+# else:
+#     print('Sorry, you can\'t.')
+
+# a = 1.0
+# print(type(a))
+
+# a = 3
+# print(a == 5.0)
+
+# b = 10
+# c = b > 9
+# print(type(c))
+# print(c)
+
+# print(5/2 == 5/2.0)
+# print(5//2 == 5/2.0)
+
+# print(10 != 2*5)
+# print(3.0-1.0 != 5.0-3.0)
+
+#print(4>5 or 3<4 and 9>8)
+# and > or
+
+# print(not 1)
+# print(not 0)
+
+# print(not 0.000001)
+# print(bool(0))
+# print(bool(0.000001))
+# print(bool('Julie'))
+# print(bool(''))
+
