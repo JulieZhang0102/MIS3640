a2a9c2d836d6064008c76df1dffa237e3290f415
JulieZhang0102 <jzhang10@babson.edu>
2019-09-26 14:12:36 -0400

    Create cal.py

---
+++ session06/cal.py
@@ -0,0 +1,63 @@
+# age = int(input('Please enter your age:'))
+
+# print(f"Your age is {age}.")
+
+# if age>=18:
+#     print("You are an adult.")
+# elif age>=10:
+#     print("You are a teenager.")
+# else:
+#     print('You are a kid.')
+
+# weight = float(input('Please enter your weight'))
+# height = float(input('Please enter your height'))
+# BMI = weight/height
+
+# if BMI <= 18.5:
+#     print('Underweight')
+# elif 18.5 < BMI < 24.9:
+#     print('Normal weight')
+# elif 25 < BMI < 29.9:
+#     print('Overweight')
+# else:
+#     print('Obesity')
+
+# import webbrowser
+
+# def compare(a, b):
+#     if isinstance(a,str) or isinstance(b,str):
+#         print('String involved')
+#     elif a > b:
+#         print('Bigger')
+#     elif a == b:
+#         print('Equal')
+#     else:
+#         print('Small')
+
+# compare(2,2)
+# compare(2,5)
+# compare(2,1)
+# compare(2,"Julie")
+
+
+# def diff21(n):
+#     if n <= 21:
+#         return 21 - n
+#     else:
+#         return 2 * (n-21)
+
+# print(diff21(25))
+
+
+# def countdown(n):
+#     import time
+#     time.sleep(1)
+
+#     if n<=0:
+#         print('Blastoff')
+#     else:
+#         print(n)
+#         countdown(n-1)
+
+# countdown(5)
+
