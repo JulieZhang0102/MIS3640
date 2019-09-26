1620364b7f0a2ae6e4aef5000e86e937a54657bf
JulieZhang0102 <jzhang10@babson.edu>
2019-09-26 16:38:17 -0400

    Create iteration.py

---
+++ session07/iteration.py
@@ -0,0 +1,57 @@
+# exercise 1
+
+# result = 0
+# for i in range (1, 11):
+#     result +=i
+#     # result = result + i
+
+# print(result)
+
+# result = 0
+# for i in range(1, 1001, 2):
+#     result += i
+
+# print(result)
+
+
+# sum of square of integers
+# result = 0
+# for i in range(1, 5):
+#     result += i*i
+
+# print(result)
+
+# name = "Greg"
+# encrypted = ""
+# for letter in name:
+#     encrypted += chr(ord(letter)+2)
+
+# print(encrypted)
+
+# def countdown(n):
+#     import time
+#     while n > 0:
+#         print(n)
+#         n-=1
+#         time.sleep(1)
+#     print('Blastoff!')
+
+# countdown(5)
+
+# a = 0
+# result = 0
+# while a < 11:
+#     print(f'Before adding {a}, sum is {result}.')
+#     result += a
+#     print(f'Adding {a}, sum is {result}.')
+#     a += 1
+#     print()
+
+# print(result)
+
+# while True:
+#     line = input('> ')
+#     if line.lower() == 'done':
+#         break
+#     print(line)
+# print('Done!')
