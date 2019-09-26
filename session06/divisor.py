ce464339712eaea3e76722e564ecdc7cb8f79b91
JulieZhang0102 <jzhang10@babson.edu>
2019-09-26 15:03:10 -0400

    Update divisor.py

--- session06/divisor.py
+++ session06/divisor.py
@@ -1,20 +1,9 @@
 def gcd(a, b):
-    if b > a:
-        small = a
-        large = b
+    if b == 0:
+        return a
     else:
-        small = b
-        large = a
-    for i in range(1, small, 1):
-        result = large%(i*small)
-        if result < small:
-            result = large%(i*small)
-        else:
-            x = large%(i*small)
-            return i
+        return gcd(b,a%b)
                     
                 
-        
-
 print(gcd(1071, 462))
 
