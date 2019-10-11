9fbb3349d48e6e33ba3fec121d39f72760ae9302
JulieZhang0102 <jzhang10@babson.edu>
2019-10-11 15:43:40 -0400

    Update dict.py

--- session11/dict.py
+++ session11/dict.py
@@ -42,3 +42,28 @@
 
 # beatles = histogram(lyrics)
 # print_hist(beatles)
+
+# def reverse_lookup(d, v):
+#     for k in d:
+#         if d[k] == v:
+#             return k
+#     raise LookupError('value does not appear in the dictionary')
+
+# h = {'Julie': 2, 'Julia': 4, 'Jennie': 8}
+# key = reverse_lookup(h, 2)
+# print(key)
+
+# def invert_dict(d):
+#     inverse = dict()
+#     for key in d:
+#         val = d[key]
+#         if val not in inverse:
+#             inverse[val] = [key]
+#         else:
+#             inverse[val].append(key)
+#     return inverse
+
+# hist = histogram('parrot')
+# print(hist)
+# inverse = invert_dict(hist)
+# print(inverse)
