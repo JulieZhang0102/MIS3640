5043ade1a222b524e094b4f5cd7a2d13500b61c3
JulieZhang0102 <jzhang10@babson.edu>
2019-11-12 16:21:39 -0500

    Create Professor.py

---
+++ session19/Professor.py
@@ -0,0 +1,19 @@
+from BabsonPerson import *
+
+class Professor(BabsonPerson):
+
+    def __init__(self, name, course):
+        BabsonPerson.__init__(self, name)
+        self.course = course
+
+    def speak(self, utterance):
+        new_utterance = f'In course {self.course}, {utterance}.'
+        return BabsonPerson.speak(self, new_utterance)
+
+def main():
+    prof = Professor('Zhi Li', 'MIS 3640')
+    print(prof.speak('Python is awesome'))
+
+
+if __name__ == "__main__":
+    main()
